e64802ee3c78f643f3238f0c2188ccd5d3174296
Angela Lam <angelakam.lam@gmail.com>
2019-08-17 14:07:37 -0700

    Updates

---
+++ precache-manifest.df2c005263d9b65fdf14613169f39368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31554b5c2e27cfe030bdd02a04074237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d57a801ace1a62e90b22",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1172c0ad0e17e9f3658d",
+    "url": "/static/js/2.3ed3d5b0.chunk.js"
+  },
+  {
+    "revision": "d57a801ace1a62e90b22",
+    "url": "/static/js/main.8a9e8270.chunk.js"
+  },
+  {
+    "revision": "23cd4f4f90a3fead4d1f",
+    "url": "/static/js/runtime~main.76a79ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
